772fb1bb0cdb5bc6337f9941cd55c921e881b5a4
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-02-23 21:07:10 -0300

    API Node com postgres

---
+++ server.js
@@ -0,0 +1,53 @@
+import { fastify } from 'fastify'
+//import { DatabaseMemory } from './database-memory.js'
+import { DatabasePostgres } from './database-postgres.js'
+
+const server = fastify()
+
+//const database = new DatabaseMemory()
+const database = new DatabasePostgres()
+
+server.post('/videos', async (request, reply) => {
+    const { title, description, duration } = request.body
+
+    await database.create({
+        title: title,
+        description: description,
+        duration: duration,
+    })
+
+    return reply.status(201).send()
+})
+
+server.get('/videos', async (request) => {
+    const search = request.query.search
+
+    const videos = await database.list(search)
+
+    return videos
+})
+
+server.put('/videos/:id', async (request, reply) => {
+    const videoId = request.params.id
+    const { title, description, duration } = request.body
+
+    await database.update(videoId, {
+        title: title,
+        description: description,
+        duration: duration,
+    })
+
+    return reply.status(204).send()
+})
+
+server.delete('/videos/:id', async (request, reply) => {
+    const videoId = request.params.id
+
+    await database.delete(videoId)
+
+    return reply.status(204).send()
+})
+
+server.listen({
+    port: process.env.PORT ?? 3000,
+}) 
